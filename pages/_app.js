5b58bb3cae38e70dfa1d9b0436094a6e9b29deab
Techno2905 <kishore29@null.net>
2023-06-14 19:59:32 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import { ProductsContextProvider } from "../components/ProductsContext";
 import "../styles/globals.css";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
      return (
-          <ProductsContextProvider>
-               <Component {...pageProps} />
-          </ProductsContextProvider>
+          <>
+               <Head>
+                    <title>Your Website Title</title>
+               </Head>
+               <ProductsContextProvider>
+                    <Component {...pageProps} />
+               </ProductsContextProvider>
+          </>
      );
 }
 
