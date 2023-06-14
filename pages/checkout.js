6a10b647bda5ed3c0e9126f9860fac3da3a5e4e1
Techno2905 <kishore29@null.net>
2023-06-14 19:18:03 +0530

    Update checkout.js

--- pages/checkout.js
+++ pages/checkout.js
@@ -251,6 +251,30 @@ export default function CheckoutPage() {
                               name="products"
                               value={selectedProducts.join(",")}
                          />
+                         <div
+                              class="flex p-4 mb-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
+                              role="alert"
+                         >
+                              <svg
+                                   aria-hidden="true"
+                                   class="flex-shrink-0 inline w-5 h-5 mr-3"
+                                   fill="currentColor"
+                                   viewBox="0 0 20 20"
+                                   xmlns="http://www.w3.org/2000/svg"
+                              >
+                                   <path
+                                        fill-rule="evenodd"
+                                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                                        clip-rule="evenodd"
+                                   ></path>
+                              </svg>
+                              <span class="sr-only">Info</span>
+                              <div>
+                                   <span class="font-medium">Alert!</span> You
+                                   can use Card number : 4242 4242 4242 4242, to
+                                   mimic a sucessful purchase on Stripe
+                              </div>
+                         </div>
                          <button
                               type="submit"
                               className="bg-primary px-5 py-3 rounded-xl text-white text-xl font-bold w-full my-4 shadow-primary shadow-md "
